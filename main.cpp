5adfdd69f2baf338ec0780ebb884235a12be6149
Alex Calugari <cross9986@gmail.com>
2017-06-09 00:18:36 +0300

    Add basic features

--- main.cpp
+++ main.cpp
@@ -2,11 +2,79 @@
 
 using namespace std;
 
+//Structures
+struct List
+{
+	int value;
+	List* next;
+};
+
+//Functions declarations
+void initialization(List*, int, int*);
+void print(List*);
+void free(List**);
+
 int main()
 {
+	List* begin = new List;
+	
+	begin -> value = 0;
+	
+	int values[5] = {10, 21, 32, 48, 56};
+	
+	initialization(begin, 5, values);
+	print(begin);
 	
 	
+	free(&begin);
 	cin.sync();
 	cin.get();
 	return 0;
 }
+
+//Functions definitions
+void initialization(List* begin, int length, int* values)
+{
+	
+	List *end = begin;
+	
+	for (int i = 0; i < length; i++)
+	{
+		end -> next = new List;
+		end = end -> next;
+		end -> value = values[i];
+	}
+	
+	end -> next = NULL;
+}
+
+void print(List* begin)
+{
+	List* printed = begin;
+	
+	while (printed)
+	{
+		cout << printed -> value << " => ";
+		printed = printed -> next;
+	}
+	
+	cout << "NULL";
+	
+}
+
+void free(List** begin)
+{
+	if (*begin == 0)
+		return;
+	
+	List* temp, * current = *begin;
+	
+	while (current)
+	{
+		temp = current;
+		current = current -> next;
+		delete temp;
+	}
+	*begin = NULL;
+}
+
