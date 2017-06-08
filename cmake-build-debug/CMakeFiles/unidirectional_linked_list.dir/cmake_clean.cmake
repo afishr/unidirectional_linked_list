a80762b2e91b02b9e11dcd34681832810005ee90
Alex Calugari <cross9986@gmail.com>
2017-06-09 00:16:59 +0300

    Set up enviroment
    CLion & CMake

---
+++ cmake-build-debug/CMakeFiles/unidirectional_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unidirectional_linked_list.dir/main.cpp.obj"
+  "unidirectional_linked_list.pdb"
+  "unidirectional_linked_list.exe"
+  "unidirectional_linked_list.exe.manifest"
+  "libunidirectional_linked_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unidirectional_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
